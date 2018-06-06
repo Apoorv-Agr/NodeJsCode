93ec333ce7fcac0c88c19e806a654e9c35b28f25
Apoorv <agrawal.av27@gmail.com>
2018-06-06 23:56:34 +0530

    Implemented get and post request

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+//mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+mongoose.connect('mongodb://127.0.0.1:27017/newDb');
 var Bear = require('./models/bear'); 
 
 app.use(bodyparser.urlencoded({extended:true}));
@@ -21,6 +22,27 @@ router.use(function(req, res, next) {
 router.get('/', function(req, res){
     res.json({message: 'horray! welcome to the API'});
 });
+
+router.route('/bears').post(function(req,res){
+    var bear = new Bear(); // Creating a new instance of the Bear Model
+    bear.name = req.body.name;
+    bear.save(function(err){
+        if(err){
+            res.send(err);
+        }
+        res.json({message:"Bear Created"});
+    });
+});
+router.route('/bears').get(function(req,res){
+    var bear = new Bear(); // Creating a new instance of the Bear Model
+    Bear.find(function(err, bears){
+        if(err){
+            res.send(err);
+        }
+        res.send(bears);
+    })
+
+})
 
 app.use('/api',router);
 
